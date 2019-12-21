ff4bad406ec25ef0f09f4033ce01d5a7c27427a3
ben33779610 <49440361+ben33779610@users.noreply.github.com>
2019-12-21 22:02:19 +0800

    界面 大略基礎

---
+++ One Man Military/Assets/Scripts/GameManger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManger : MonoBehaviour
+{
+	/// <summary>
+	/// 開始遊戲，選擇關卡
+	/// </summary>
+	public void GameStart()
+	{
+		SceneManager.LoadScene("選擇關卡");
+	}
+
+	public void Exit()
+	{
+		Application.Quit();
+	}
+
+}
